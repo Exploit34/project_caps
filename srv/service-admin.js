a71fd9bbce7062661cffa14ded1b7a5f48eb0522
Exploit34 <josefexner456@gmail.com>
2024-04-16 12:04:16 -0500

    back-end-1.2v cambios archivo js

--- srv/service-admin.js
+++ srv/service-admin.js
@@ -5,11 +5,10 @@ export default (srv) => {
 
     srv.on('READ', User, async (req) => {
         try {
-            const users= await cds.run(SELECT.from(User));
-
-            req.reply(users);
+            const users = await cds.run(SELECT.from(User));
+            return users;
         } catch (error) {
-            req.error(500, 'Error al leer los datos');
+            req.error(500, 'Error al leer los datos', error);
         }
     });
 
@@ -26,35 +25,41 @@ export default (srv) => {
 
             if (existeData.length > 0) return req.error(400, 'El correo electrónico ya existe');
 
-            await cds.transaction(req).run(
+            await cds.run(
                 INSERT.into(User).entries(datauser)
-            )
+            );
+
             req.info(201, 'Usuario creado exitosamente');
         } catch (error) {
-            req.error(500, 'Error al crear al usario', error)
+            req.error(500, 'Error al crear al usario', error);
         }
     });
 
-    srv.on('PUT', User, async (req) => {
+    srv.on('UPDATE', User, async (req) => {
         try {
             const datauser = req.data;
             const id = req.params[0].ID;
-            return await cds.run(
-                UPDATE(User).set(datauser).where({ID: id})
+            await cds.run(
+                UPDATE(User).set(datauser).where({ ID: id })
             );
+
+            req.info(200, 'Usuario actualizado exitosamente');
         } catch (error) {
-            req.error(500, 'Error al actualizar el usuario')
+            req.error(500, 'Error al actualizar el usuario', error);
         }
     });
 
     srv.on('DELETE', User, async (req) => {
         try {
             const id = req.params[0].ID;
-            return await cds.run(
+
+            await cds.run(
                 DELETE.from(User).where({ ID: id })
             );
+
+            req.info(200, 'Usuario eliminado exitosamente');
         } catch (error) {
-            req.error(500, 'Error al actualizar el usuario')
+            req.error(500, 'Error al actualizar el usuario'. error);
         }
     });
 };
